fceb8492e8c6ac6e993bebdf54a4c4f35ca65cfa
Sabyata Karki <sabyatakarki05@com>
2026-01-05 17:42:28 +0545

    tested

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-// src/index.ts
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import { connectDatabase } from './database/db'; // your MongoDB connection
-import { PORT } from './config'; // ensure PORT is exported from config.ts
-import authRoutes from './routes/auth.route'; // your auth routes
+import { connectDatabase } from './database/db'; 
+import { PORT } from './config';
+import authRoutes from './routes/auth.route'; 
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -11,14 +10,10 @@ console.log(process.env.PORT);
 
 const app: Application = express();
 
-// Middleware
-// ---------------------
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ---------------------
-// Routes
-// ---------------------
+
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req: Request, res: Response) => {
@@ -28,20 +23,17 @@ app.get('/', (req: Request, res: Response) => {
   });
 });
 
-// ---------------------
-// Start Server
-// ---------------------
+
 async function startServer() {
   console.log("Starting server...");
 
-  // Connect to MongoDB
+
   await connectDatabase();
 
-  // Start Express server
+
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
 
-// Run server
 startServer();
