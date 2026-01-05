2dd58cf864ae74ecbb9cf3edffc8eaad5e0e3c3f
Sabyata Karki <sabyatakarki05@com>
2026-01-05 17:34:34 +0545

    added dependencies

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -6,7 +6,7 @@ export const UserSchema = z.object({
     password: z.string().min(6),
     firstName: z.string().optional(),
     lastName: z.string().optional(),
-    role: z.enum(["user", "admin"]).default("user"),
+    role: z.enum(["user"]).default("user"),
 });
 
 export type UserType = z.infer<typeof UserSchema>;
